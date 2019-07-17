a3091cc71657c19ac5df99e60efee3fcb7bb4140
Sergio Crisostomo <sergiosbox@gmail.com>
2019-07-17 14:17:00 +0200

    refactor locic in requestStore to use instance and endpoints

--- src/requestsStore.js
+++ src/requestsStore.js
@@ -6,6 +6,7 @@ const actions = {
     if (store.getters.registeredComponents.get(instance)) {
       return;
     }
+
     instance.$once('hook:beforeDestroy', () => {
       store.commit('unregisterComponent', instance);
     });
@@ -17,8 +18,8 @@ const actions = {
   unregisterComponentInStore(store, instance) {
     store.commit('unregisterComponent', instance);
   },
-  unregisterRequest(store, ref) {
-    store.commit('unregisterRequest', ref);
+  unregisterRequest(store, req) {
+    store.commit('unregisterRequest', req);
   },
   updateRequest(store, req) {
     store.commit('updateRequest', req);
@@ -29,7 +30,9 @@ const mutations = {
   registerComponent(state, instance) {
     state.registeredComponents.set(instance, []);
   },
-  registerRequest(state, {logEndpoints, logInstance, request, callerInstance}) {
+  registerRequest(state, request) {
+    const {logEndpoints, logInstance, endpoint, callerInstance} = request;
+
     // register by component instance
     if (logInstance) {
       const instanceRequests = state.registeredComponents.get(callerInstance) || [];
@@ -39,10 +42,10 @@ const mutations = {
 
     // register by endpoint
     if (logEndpoints) {
-      const current = state.activeRequestsToEndpoint[request.endpoint] || [];
+      const current = state.activeRequestsToEndpoint[endpoint] || [];
       state.activeRequestsToEndpoint = {
         ...state.activeRequestsToEndpoint,
-        [request.endpoint]: current.concat(request),
+        [endpoint]: current.concat(request),
       };
     }
   },
@@ -53,12 +56,13 @@ const mutations = {
 
     state.registeredComponents.set(instance, null); // maybe redundant but the idea is to help clearing memory
     state.registeredComponents.delete(instance);
+
     if (state.lastUpdatedComponent === instance) {
       state.lastUpdatedComponent = null;
     }
   },
   unregisterRequest(state, request) {
-    const {id, endpoint} = request;
+    const {id, endpoint, callerInstance} = request;
 
     // unregister endpoint
     const activeRequestsToEndpointPredicate = function activeRequestsToEndpointPredicate(req) {
@@ -72,33 +76,64 @@ const mutations = {
       ...state.activeRequestsToEndpoint,
       [endpoint]: others,
     };
+
+    // update component endpoint list
+    const instanceRequests = state.registeredComponents.get(callerInstance);
+
+    if (instanceRequests) {
+      const removeIdIterator = function removeIdIterator(req) {
+        return req.id !== id;
+      };
+
+      const requestList = instanceRequests.filter(removeIdIterator);
+      state.registeredComponents.set(callerInstance, requestList);
+    }
   },
-  updateRequest(state, req) {
-    return;
+  updateRequest(state, request) {
+    const {id, logInstance, logEndpoints, endpoint, callerInstance} = request;
+
+    state.lastUpdatedComponent = callerInstance;
+
+    const requestUpdateIterator = function requestUpdateIterator(req) {
+      const updatedRequest = id === req.id ? request : req;
+
+      return updatedRequest;
+    };
 
     // Since we cannot use listener for complex/nested objects
     // we use a shallow state key that triggers listeners in components
     // and they can check if the change is related to them or ignore the call
-    state.lastUpdatedComponent = req.logInstance;
-    const componentRequests = state.registeredComponents.get(req.logInstance) || [];
-    const componentRequestsPredicate = function componentRequestsPredicate(r) {
-      return r.id === req.id && r.uuid === req.uuid;
-    };
 
-    const index = componentRequests.findIndex(componentRequestsPredicate);
+    // update the component instance list
+    const instanceRequests = state.registeredComponents.get(callerInstance);
 
-    if (index === -1) {
-      console.info('store mutations > updateRequest: Request not found in store');
+    if (logInstance) {
+      if (instanceRequests) {
+        // sometimes we have removed the component before the request is updated
+        // in such cases we should not re-add the instance to the list
+
+        const requestList = (instanceRequests || []).map(requestUpdateIterator);
+        state.registeredComponents.set(callerInstance, requestList);
+      }
     }
 
-    const componentRequestsIteratee = function componentRequestsIteratee(entry, i) {
-      return index === i ? req : entry;
-    };
+    // update the endpoint list
+    if (logEndpoints) {
+      const current = state.activeRequestsToEndpoint[endpoint];
 
-    state.registeredComponents = {
-      ...state.registeredComponents,
-      [req.uuid]: componentRequests.map(componentRequestsIteratee),
-    };
+      if (!current) {
+        console.info('VRR store mutations > updateRequest: Request not found in store');
+
+        return;
+      }
+
+      const requestList = current.map(requestUpdateIterator);
+
+      state.activeRequestsToEndpoint = {
+        ...state.activeRequestsToEndpoint,
+        [endpoint]: requestList,
+      };
+    }
   },
 };
 
